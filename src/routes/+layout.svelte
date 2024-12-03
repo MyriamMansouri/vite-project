6c4e3d8d1b5c80cc0c84a11ef48d8955111e1fa2
Myriam Mansouri <myriam.mansouri@radio-canada.ca>
2024-12-03 17:42:44 -0500

    fix: tailwind not working

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-  import Layout from '$lib/Layout.svelte';
-  let cards: { i: number; x: number; y: number }[] = $state([]);
+	import '../app.css';
+	import Layout from '$lib/Layout.svelte';
+	let cards: { i: number; x: number; y: number }[] = $state([]);
 
-  function addCard(e: MouseEvent) {
-    cards = [...cards, { x: e.clientX, y: e.clientY, i: cards.length + 1 }];
-  }
+	function addCard(e: MouseEvent) {
+		cards = [...cards, { x: e.clientX, y: e.clientY, i: cards.length + 1 }];
+	}
 </script>
 
 <svelte:window on:dblclick={addCard} />
 
 <main>
-  <Layout {cards} />
+	<Layout {cards} />
 </main>
